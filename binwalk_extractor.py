e7e9fec7a46c6802ef6810b415b81c91874fdb28
Scott Glover <scottgl@gmail.com>
2018-10-23 17:28:58 -0500

    Moved main code to binwalk_extract().

--- binwalk_extractor.py
+++ binwalk_extractor.py
@@ -6,18 +6,11 @@ import argparse
 import json
 import lzma
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', action='store', dest='filename',
-                        default='filename.bin',
-                        help='filename of firmware to analyze')
-
-    results = parser.parse_args()
-
+def binwalk_extract(filename, path):
     offsets = []
     descriptions = []
 
-    for module in binwalk.scan(results.filename, signature=True, quiet=True):
+    for module in binwalk.scan(filename, signature=True, quiet=True):
         for result in module.results:
             offsets.append(result.offset)
             descriptions.append(result.description)
@@ -45,12 +38,25 @@ if __name__ == '__main__':
         elif "PC bitmap" in descriptions[i]:
             ext = "bmp"
         outname="{}.{}".format(i, ext)
+        outpath="{}/{}".format(path, outname)
         desc[outname] = { 'start': o1, 'end': o2, 'info': descriptions[i] }
         part = data[o1:o2]
         with open(outname, "wb") as binary_file:
             binary_file.write(part)
         if ext == "7z":
             os.system("lzcat -q -q {} > {}".format(outname, "{}.bin".format(i)))
-
+            os.mkdir("{}".format(i))
     with open("info.json", "w") as f:
         f.write(json.dumps(desc))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', action='store', dest='filename',
+                        default='filename.bin',
+                        help='filename of firmware to analyze')
+
+    results = parser.parse_args()
+
+    binwalk_extract(results.filename, ".")
+
